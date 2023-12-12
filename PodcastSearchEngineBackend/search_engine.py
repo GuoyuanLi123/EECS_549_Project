9e2efc92c226cde576c4f78bf13c90a617b95780
Guoyuan <130336982+GuoyuanLi123@users.noreply.github.com>
2023-12-12 15:42:18 -0500

    Add files via upload

--- PodcastSearchEngineBackend/search_engine.py
+++ PodcastSearchEngineBackend/search_engine.py
@@ -6,11 +6,10 @@ import os
 
 class SearchEngine:
     def __init__(self, dataset_path: str ):
-        if not pt.started():
-            pt.init()
         self.dataset_path = dataset_path
         self.indexref = None
         self.index = None
+        self.ranker = None
 
     def iter_file(self):
         with open(self.dataset_path, 'rt') as file:
@@ -20,17 +19,34 @@ class SearchEngine:
                 yield json.loads(l)
 
     def build_index(self):
+        if not pt.started():
+            pt.init()
+
         if os.path.exists("index"):
             self.index = pt.IndexFactory.of("./index")
             self.indexref = pt.IndexRef.of("./index/data.properties")
         else:
-            self.indexref = pt.IterDictIndexer("./index", meta={"docno": 20, "episode_uri":1024, "episode_name":1024, "show_name":1024, "transcript":256}).index(
+            self.indexref = pt.IterDictIndexer("./index", meta={"docno": 20, "publisher":128, "episode_url":1024, "episode_name":1024, "show_name":1024, "transcript":256}).index(
                 self.iter_file(),
                 fields=("transcript",))
             self.index = pt.IndexFactory.of(self.indexref)
 
-    def filter(self, result):
-        pass
+        self.ranker = pt.BatchRetrieve(self.index, wmodel="BM25", metadata=["episode_url", "episode_name", "show_name", "publisher", "transcript"])
 
-    def query(self, query: str, episodeName: str, showName:str, publisher:str):
-        pass
+    
+    def query(self, query: str, episodeName='', showName='', publisher=''):
+        result = self.ranker.search(query).drop(columns=['qid', 'docid', 'rank', 'score', 'query'])
+        if episodeName:
+            result = result[result['episode_name'] == episodeName]
+        
+        if showName:
+            result = result[result['show_name'] == showName]
+
+        if publisher:
+            result = result[result['publisher'] == publisher]
+
+        return result.to_dict('records')
+
+        
+
+
